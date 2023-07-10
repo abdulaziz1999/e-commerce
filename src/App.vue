5c890006baace626357661cf7a84d0ed7cc4048a
IHC <IHC@PBM00103210241.pertamina.com>
2023-07-10 09:21:01 +0700

    first commit vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    Footer
+  }
+}
+</script>
+<style></style>
