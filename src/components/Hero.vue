d1530199350338a07bafdbe2e08204559548bcd5
IHC <IHC@PBM00103210241.pertamina.com>
2023-07-11 13:22:05 +0700

    component card

---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hero min-h-600 bg-base-200">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">Sepatu Shop</h1>
+                <p class="py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta accusamus officia fuga neque odit repellendus tempore minus corporis a, possimus iure? Esse magni sapiente vero, impedit cupiditate enim distinctio facilis?</p>
+                <router-link to="/products" class="btn sm:btn-sm md:btn-md lg:btn-md font-serif btn-success">Belanja Sekarang !</router-link >
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeroComp',
+}
+</script>
+
+<style>
+</style>
